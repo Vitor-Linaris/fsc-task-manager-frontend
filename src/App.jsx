b93ab8d0560d16b7315f6235ea3154771d3ba444
Vitor-Linaris <linaris@DESKTOP-D7AD2JN>
2024-07-19 22:18:49 -0300

    add project finally for repository

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+
+import "./App.scss";
+
+const App = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+        </Routes>
+    );
+};
+
+export default App;
---
+++ src/components/Tasks.jsx
@@ -0,0 +1,71 @@
+import { useState, useEffect, useMemo, useCallback } from "react";
+import axios from "axios";
+import { useAlert } from "react-alert";
+
+import "./Tasks.scss";
+
+import TaskItem from "./TaskItem";
+import AddTask from "./AddTask";
+
+const Tasks = () => {
+    const [tasks, setTasks] = useState([]);
+
+    const alert = useAlert();
+
+    const fetchTasks = useCallback(async () => {
+        try {
+            const { data } = await axios.get(`http://localhost:8000/tasks`);
+
+            setTasks(data);
+        } catch (_error) {
+            alert.error("Não foi possível recuperar as tarefas.");
+        }
+    }, [alert]);
+
+    const lastTasks = useMemo(() => {
+        return tasks.filter((task) => task.isCompleted === false);
+    }, [tasks]);
+
+    const completedTasks = useMemo(() => {
+        return tasks.filter((task) => task.isCompleted === true);
+    }, [tasks]);
+
+    useEffect(() => {
+        fetchTasks();
+    }, [fetchTasks]);
+
+    return (
+        <div className="tasks-container">
+            <h2>Minhas Tarefas</h2>
+
+            <div className="last-tasks">
+                <h3>Últimas Tarefas</h3>
+                <AddTask fetchTasks={fetchTasks} />
+                <div className="tasks-list">
+                    {lastTasks.map((lastTask) => (
+                        <TaskItem
+                            key={lastTask._id}
+                            task={lastTask}
+                            fetchTasks={fetchTasks}
+                        />
+                    ))}
+                </div>
+            </div>
+
+            <div className="completed-tasks">
+                <h3>Tarefas Concluídas</h3>
+                <div className="tasks-list">
+                    {completedTasks.map((completedTask) => (
+                        <TaskItem
+                            key={completedTask._id}
+                            task={completedTask}
+                            fetchTasks={fetchTasks}
+                        />
+                    ))}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Tasks;
